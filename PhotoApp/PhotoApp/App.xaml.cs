8ee591524e1edb67aeb60aea6d21b398c855df95
Kobe ponet <ponetkobe@gmail.com>
2022-05-10 09:39:56 +0200

    changed startup file

--- PhotoApp/PhotoApp/App.xaml.cs
+++ PhotoApp/PhotoApp/App.xaml.cs
@@ -12,7 +12,7 @@ namespace PhotoApp
 
             Plugin.Media.CrossMedia.Current.Initialize();
 
-            MainPage = new EventPage();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
