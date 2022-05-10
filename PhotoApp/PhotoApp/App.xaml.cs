0d7695d8f70bb88060dc2b4ba247461ae3fc900e
Kobe ponet <ponetkobe@gmail.com>
2022-05-10 09:43:25 +0200

    startup

--- PhotoApp/PhotoApp/App.xaml.cs
+++ PhotoApp/PhotoApp/App.xaml.cs
@@ -12,7 +12,7 @@ namespace PhotoApp
 
             Plugin.Media.CrossMedia.Current.Initialize();
 
-            MainPage = new MakeEventPage();
+            MainPage = new EventPage();
         }
 
         protected override void OnStart()
