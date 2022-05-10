35d7586399eb0b0063265fad04cfa6c556b98530
SDM2004 <Stefke.De.Meyer@hotmail.com>
2022-05-10 09:38:17 +0200

    Naming variables

--- PhotoApp/PhotoApp/MakeEventPage.xaml.cs
+++ PhotoApp/PhotoApp/MakeEventPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +14,11 @@ namespace PhotoApp
         public MakeEventPage()
         {
             InitializeComponent();
+        }
+
+        private void btnEventMaken_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
