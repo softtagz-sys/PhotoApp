0d6023a18903ac2b559968bfd6a8a98cfe4363d8
Jan Hermans <hermansjan@telenet.be>
2022-06-05 22:20:49 +0200

    test

---
+++ PhotoApp/PhotoApp/Interfaces/IDBConnectorInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoApp.Interfaces
+{
+    internal interface IDBConnectorInterface
+    {
+        public string getMySQLColumns();
+        public string getMySQLValues();
+    }
+}
